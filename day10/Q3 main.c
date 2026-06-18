cca0556534893f59fa760be170f6fd04555303c9
shouryatiwari777 <m9tgtw4zb9@privaterelay.appleid.com>
2026-06-18 22:20:22 +0530

    Add files via upload

---
+++ day10/Q3 main.c
@@ -0,0 +1,32 @@
+Write a program to Print number pyramid.
+    1
+   121
+  12321
+ 1234321
+123454321
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        // Print leading spaces
+        for (int j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+
+        // Print ascending numbers
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Print descending numbers
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
