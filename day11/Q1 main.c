542950b5861eca37f290dc8a3bd27d49e3c9843b
shouryatiwari777 <m9tgtw4zb9@privaterelay.appleid.com>
2026-06-19 22:43:37 +0530

    Add files via upload

---
+++ day11/Q1 main.c
@@ -0,0 +1,18 @@
+Write a program to Write function to find sum of two numbers
+#include <stdio.h>
+
+// Function to find sum of two numbers
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Sum = %d\n", sum(num1, num2));
+
+    return 0;
+}
