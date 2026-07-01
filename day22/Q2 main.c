75f018edb8165cf4fb36c44a87569e63cf4b0de0
shouryatiwari777 <m9tgtw4zb9@privaterelay.appleid.com>
2026-07-01 22:16:02 +0530

    Add files via upload

---
+++ day22/Q2 main.c
@@ -0,0 +1,19 @@
+Write a program to Count words in a sentence.
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ' ' && str[i + 1] != ' ')
+            words++;
+    }
+
+    printf("Number of words = %d\n", words);
+
+    return 0;
+}
